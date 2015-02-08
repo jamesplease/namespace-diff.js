389e83cf96d8032b00bae29c5552597ff530ca79
James Smith <jellyes2@gmail.com>
2015-02-08 12:18:28 -0500

    Minor code tweaks.

--- src/namespace-diff.js
+++ src/namespace-diff.js
@@ -1,13 +1,15 @@
 function namespaceDiff(start, end, sep = '.') {
   if (start === end) {
-    return {outStates: [], inStates: []};
+    return { outStates: [], inStates: [] };
   }
 
   // Determine the state up to which they are the same
   var startStates = start.split(sep);
   var endStates = end.split(sep);
   for (var i = 0; i < startStates.length; i++) {
-    if (startStates[i] !== endStates[i]) {  break; }
+    if (startStates[i] !== endStates[i]) {
+      break;
+    }
   }
 
   // Use that information to generate the result
